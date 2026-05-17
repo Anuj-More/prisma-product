38d523b94cae5564db97ce7a04df1ad07d759b66
Anuj More <work.anujmore@gmail.com>
2026-05-17 23:16:02 +0530

    setup and initialise prisma and the Product table

---
+++ prisma/migrations/20260517162944_init_product_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NULL,
+    `sale` BOOLEAN NOT NULL DEFAULT false,
+    `availability` ENUM('ONLINE', 'IN_STORE') NOT NULL DEFAULT 'ONLINE',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
