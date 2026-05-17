82cfb116475a9b4c58b6e6f5775e58941427cfa9
Anuj More <work.anujmore@gmail.com>
2026-05-18 00:37:12 +0530

    create product resource scaffolding

---
+++ src/product/product.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
+import { ProductCreateInput, ProductUpdateInput } from '../generated/prisma/models';
+
+@Injectable()
+export class ProductService {
+    constructor(private readonly dbService: DatabaseService) {}
+
+    create(productCreateInput: ProductCreateInput) {
+        return 'This action adds a new product';
+    }
+
+    findAll() {
+        return `This action returns all product`;
+    }
+
+    findOne(id: number) {
+        return `This action returns a #${id} product`;
+    }
+
+    update(id: number, updateProductDto: ProductUpdateInput) {
+        return `This action updates a #${id} product`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} product`;
+    }
+}
