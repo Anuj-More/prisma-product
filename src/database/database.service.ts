f4c6d40a82061737347470bc27fe7578f076c16f
Anuj More <work.anujmore@gmail.com>
2026-05-18 22:44:42 +0530

    setup mysql adapter for prisma

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -5,6 +5,7 @@ import {
     OnModuleInit,
 } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Global()
 @Injectable()
@@ -12,6 +13,17 @@ export class DatabaseService
     extends PrismaClient
     implements OnModuleInit, OnModuleDestroy
 {
+    constructor() {
+        const adapter = new PrismaMariaDb({
+            user: 'root',
+            password: process.env.MYSQL_ROOT_PASSWORD,
+            database: process.env.MYSQL_DATABASE_NAME,
+            connectionLimit: 5,
+        });
+
+        super({ adapter });
+    }
+
     async onModuleInit() {
         await this.$connect();
     }
